08ebcaab52e6faf8baf5a65af3bf3a370b35e757
bocky2 <pokedoug@hotmail.com.au>
2017-07-31 21:14:55 +1000

    Beginning of maps
    Maps have been started working on, tilemaps and animations for water tiles also created.

---
+++ Assets/Textures/Tilemaps/stone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="stone" tilewidth="20" tileheight="20" tilecount="15" columns="5">
+ <image source="stone.png" width="100" height="60"/>
+</tileset>
---
+++ Assets/Textures/Tilemaps/water4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="water4" tilewidth="20" tileheight="20" tilecount="20" columns="4">
+ <image source="water4.png" width="80" height="100"/>
+</tileset>
