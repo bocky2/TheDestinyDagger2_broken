be8a1141062fff61ef68de91b15c4c2e21e60e64
bocky2 <pokedoug@hotmail.com.au>
2017-08-03 20:32:57 +1000

    Map application
    Map put in game and player is moving well

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Setting a variable for the player's rigidbody
+    Rigidbody2D rbody;
+    //Setting a variable for speed
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Getting the player's rigidbody and assigning it to variable
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculating value that decides whether play is moving or not,
+        //by detecting input on the horizontal and vertical axes
+        Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"),
+                                            Input.GetAxisRaw("Vertical"));
+        //Move the player
+        rbody.MovePosition(rbody.position + movementVector * Time.deltaTime * speed);
+    }
+}
