04fa839c70b14152e36eac7398c6049183214110
bocky2 <pokedoug@hotmail.com.au>
2017-08-27 21:56:10 +1000

    ranger animations created, movement revisited
    All ranger animations that can be made from done art have been created, movement has been revisted to make it more simple.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -19,11 +19,39 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Calculating value that decides whether play is moving or not,
+        //if the up arrow key is pressed
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            //move the player upwards
+            transform.Translate(Vector2.up * speed);
+        }
+        //if the down arrow key is pressed
+        else if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            //move the player downwards
+            transform.Translate(Vector2.down * speed);
+        }
+        //if the right arrow key is pressed
+        else if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            //move the player right
+            transform.Translate(Vector2.right * speed);
+        }
+        //if the left arrow key is pressed
+        else if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            //move the player left
+            transform.Translate(Vector2.left * speed);
+        }
+
+        // -Alternative method \/
+        /*
+        //Calculating value that decides whether player is moving or not,
         //by detecting input on the horizontal and vertical axes
         Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"),
                                             Input.GetAxisRaw("Vertical"));
         //Move the player
         rbody.MovePosition(rbody.position + movementVector * Time.deltaTime * speed);
+        */
     }
 }
