d0b80702194a360814ae676b5ff64026c10ee183
bocky2 <pokedoug@hotmail.com.au>
2017-08-28 21:09:30 +1000

    Continued animations
    Triggers (bools) made and implemented in movement script. Simple animation in progress

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     //Setting a variable for the player's rigidbody
     Rigidbody2D rbody;
+    //Setting a variable for the player's animator
+    Animator anim;
     //Setting a variable for speed
     public float speed;
 
@@ -14,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     {
         //Getting the player's rigidbody and assigning it to variable
         rbody = GetComponent<Rigidbody2D>();
+        //Getting the player's animator and assigning it to variable
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,24 +28,51 @@ public class PlayerMovement : MonoBehaviour
         {
             //move the player upwards
             transform.Translate(Vector2.up * speed);
+
+            anim.SetBool("runU", true);
+            anim.SetBool("runD", false);
+            anim.SetBool("runR", false);
+            anim.SetBool("runL", false);
         }
         //if the down arrow key is pressed
         else if (Input.GetKey(KeyCode.DownArrow) == true)
         {
             //move the player downwards
             transform.Translate(Vector2.down * speed);
+
+            anim.SetBool("runU", false);
+            anim.SetBool("runD", true);
+            anim.SetBool("runR", false);
+            anim.SetBool("runL", false);
         }
         //if the right arrow key is pressed
         else if (Input.GetKey(KeyCode.RightArrow) == true)
         {
             //move the player right
             transform.Translate(Vector2.right * speed);
+
+            anim.SetBool("runU", false);
+            anim.SetBool("runD", false);
+            anim.SetBool("runR", true);
+            anim.SetBool("runL", false);
         }
         //if the left arrow key is pressed
         else if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
             //move the player left
             transform.Translate(Vector2.left * speed);
+
+            anim.SetBool("runU", false);
+            anim.SetBool("runD", false);
+            anim.SetBool("runR", false);
+            anim.SetBool("runL", true);
+        }
+        else
+        {
+            anim.SetBool("runU", false);
+            anim.SetBool("runD", false);
+            anim.SetBool("runR", false);
+            anim.SetBool("runL", false);
         }
 
         // -Alternative method \/
